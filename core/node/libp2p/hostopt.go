6528a09b3c7b0a454aa58a80fba88ddc1106643d
Michael Kanevsky <michael@adaptivelogic.net>
2019-10-26 22:08:48 +0300

    redundant namespace

--- core/node/libp2p/hostopt.go
+++ core/node/libp2p/hostopt.go
@@ -3,8 +3,6 @@ package libp2p
 import (
 	"context"
 	"fmt"
-	"github.com/libp2p/go-libp2p"
-
 	"paidpiper.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-core/host"
 	peer "github.com/libp2p/go-libp2p-core/peer"
